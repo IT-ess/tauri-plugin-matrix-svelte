73749634fd4c6bc84cabb4e35e445be950ae2556
Itess <me@aloiseau.com>
2025-06-09 12:52:18 +0200

    chore: update to shadcn 1.0 + prettier

--- example/matrix-svelte-client/src/routes/+layout.svelte
+++ example/matrix-svelte-client/src/routes/+layout.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-  import "../app.css";
-  import { goto } from "$app/navigation";
-  let { children, data } = $props();
+	import '../app.css';
+	import { goto } from '$app/navigation';
+	let { children, data } = $props();
 
-  $effect(() => {
-    if (data.loginStore.state.state == "awaitingForLogin") {
-      goto("/login");
-    } else {
-      data.loginStore.state.state = "loggedIn";
-    }
-  });
+	$effect(() => {
+		if (data.loginStore.state.state == 'awaitingForLogin') {
+			goto('/login');
+		} else {
+			data.loginStore.state.state = 'loggedIn';
+		}
+	});
 </script>
 
 {@render children()}
 
 <p>
-  Current status: {data.roomsCollection.state.status.status}. Message: {data
-    .roomsCollection.state.status.message}
+	Current status: {data.roomsCollection.state.status.status}. Message: {data.roomsCollection.state
+		.status.message}
 </p>
--- example/matrix-svelte-client/src/routes/+layout.ts
+++ example/matrix-svelte-client/src/routes/+layout.ts
@@ -1,15 +1,10 @@
-import {
-  loginStore,
-  roomsCollection,
-  roomStoresMap,
-  storeListener,
-} from "../hooks.client";
-import type { LayoutLoad } from "./$types";
+import { loginStore, roomsCollection, roomStoresMap, storeListener } from '../hooks.client';
+import type { LayoutLoad } from './$types';
 
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const prerender = true;
 export const ssr = false;
 
 export const load: LayoutLoad = () => {
-  return { roomsCollection, roomStoresMap, storeListener, loginStore };
+	return { roomsCollection, roomStoresMap, storeListener, loginStore };
 };
