06cf94f4f5daf551e75b33bc9b1edd1b1aa346ad
Itess <me@aloiseau.com>
2025-07-21 17:51:02 +0200

    chore: update android project

--- example/matrix-svelte-client/src-tauri/gen/android/app/src/main/java/com/matrix_svelte/client/MainActivity.kt
+++ example/matrix-svelte-client/src-tauri/gen/android/app/src/main/java/com/matrix_svelte/client/MainActivity.kt
@@ -1,29 +1,11 @@
 package com.matrix_svelte.client
 
-//import android.content.ContentValues.TAG
-//import android.os.Bundle
-//import android.util.Log
-//import android.widget.Toast
-//import com.google.android.gms.tasks.OnCompleteListener
-//import com.google.firebase.messaging.FirebaseMessaging
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 
-class MainActivity : TauriActivity() 
-//{
-//  override fun onCreate(savedInstanceState: Bundle?) {
-//    super.onCreate(savedInstanceState)
-//
-//    FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-//      if (!task.isSuccessful) {
-//        Log.w("FCM", "Fetching FCM registration token failed", task.exception)
-//        return@OnCompleteListener
-//      }
-//
-//      // Get new FCM registration token
-//      val token = task.result
-//
-//      
-//      Log.d("FCM", token)
-//      Toast.makeText(baseContext, token, Toast.LENGTH_LONG).show()
-//    })
-//  }
-//}
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
