d44fab8fa014d025b54fc17f39b2f54b556b118b
Itess <me@aloiseau.com>
2025-07-23 09:21:24 +0200

    feat: tray icon on desktop

--- example/matrix-svelte-client/src-tauri/src/lib.rs
+++ example/matrix-svelte-client/src-tauri/src/lib.rs
@@ -1,3 +1,8 @@
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     #[cfg(debug_assertions)] // only enable instrumentation in development builds
@@ -27,6 +32,25 @@ pub fn run() {
                 .build(),
         )
         .plugin(tauri_plugin_matrix_svelte::init())
+        .setup(|app| {
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
+            let _tray = TrayIconBuilder::new()
+              .icon(app.default_window_icon().unwrap().clone())
+              .menu(&menu)
+              .show_menu_on_left_click(true)
+              .on_menu_event(|app, event| match event.id.as_ref() {
+                "quit" => {
+                  println!("quit menu item was clicked");
+                  app.exit(0);
+                }
+                _ => {
+                  println!("menu item {:?} not handled", event.id);
+                }
+              })
+              .build(app)?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
