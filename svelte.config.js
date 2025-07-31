e3d8fac1d896f555c89bb480ffd95770777f8998
Itess <me@aloiseau.com>
2025-07-31 10:43:40 +0200

    chore: prettier

--- svelte.config.js
+++ svelte.config.js
@@ -16,11 +16,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-	files: {
-	  lib: "guest-js/lib",
-			appTemplate: "guest-js/app.html",
-			routes: "guest-js/routes"
-	}
+		files: {
+			lib: 'guest-js/lib',
+			appTemplate: 'guest-js/app.html',
+			routes: 'guest-js/routes'
+		}
 	}
 };
 
