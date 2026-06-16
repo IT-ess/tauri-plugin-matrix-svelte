8a18444a996911eaa2783336466c492d82623e36
Itess <me@aloiseau.com>
2026-06-16 20:50:39 +0200

    fix: regenerate project with latest cli

--- example/matrix-svelte-client/src-tauri/gen/android/app/build.gradle.kts
+++ example/matrix-svelte-client/src-tauri/gen/android/app/build.gradle.kts
@@ -118,6 +118,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation ("rustls:rustls-platform-verifier:0.1.1")
+    implementation("androidx.lifecycle:lifecycle-process:2.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
